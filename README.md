22f7a2f3b199355e0769c714f63d494613b5326c
jotaalvim <joaoafonsoalvim@gmail.com>
2023-02-01 01:08:54 +0000

    duplicated files, readme update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### jotaalvim-tool
+a collection of small and simple scrips made by me
 
 ## ddl - Delete duplicated lines
 Delete repeated lines in file, ignores empty lines and "empty" lines who have ``` `{ }``` in them. Similar to
@@ -10,12 +12,13 @@ sort -u
 ```
 however, using this tool you keep the same relative order in the file.
 
+## ddf - Delete duplicated files
+uses md5 hash to find and delete duplicates files
 
-## Work in progress 
-    md5 file converter
-    ddf
+Avalibale options 
+* -r, recursive mode 
+* -h, help
+
 
 ## todo
 sort teste | uniq -c | sort -n
-delete duplicated files
-    • md5
--- ddf.hs
+++ ddf.hs
@@ -1,10 +1,38 @@
 import System.Environment
 import qualified Data.ByteString.Lazy as LB
-import Data.Digest.Pure.MD5
+import Crypto.Hash
+
+import System.Directory
+
+data Mode = Recursive | Local | Help deriving Show
+
+valid_options = ["-r","-h"]
 
 main :: IO ()
 main = do
     args <- getArgs
-    fileContent <- LB.readFile "foo.txt"
-    let md5Digest = md5 fileContent
+    case (length args) of
+        1 -> do 
+            let path = head args
+            case path of
+                "-h" -> putStrLn "help"
+                _    -> ddf path Local
+        2 -> do 
+            let [option,path] = args
+            case (elem option valid_options) of
+                True  -> ddf path Recursive
+                False -> putStrLn "Invalid Option, ddf -h for help"
+
+        _ -> putStrLn "Invalid command, please only give one directory"
+
+ddf :: FilePath -> Mode -> IO()
+ddf path Recursive = do
+    print "modo recursivo"
+
+ddf path Local = do
+    --files <- listDirectory path
+    --print files
+    content <- LB.readFile path
+    let md5Digest = md5 content 
     print md5Digest
+    -- removeFile :: FilePath -> IO ()
